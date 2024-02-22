bcf734621fe311f9fff76971fbbd38dc579cab01
Luan-Vn4 <luanvilaca1@gmail.com>
2024-02-22 18:21:59 -0300

    primeiro commit

---
+++ src/main/java/br/upe/bulaexpress/BulaExpressApplication.java
@@ -0,0 +1,13 @@
+package br.upe.bulaexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/bulaexpress/BulaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.bulaexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
